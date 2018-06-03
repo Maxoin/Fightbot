c8d0a33af31e33f7d79f0a70111eca8017b3d4fd
Maxoin <39875172+Maxoin@users.noreply.github.com>
2018-06-03 11:35:05 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+var randnum = 0;
+
+var prefix = ("!");
+
+bot.on('ready',() => {
+    console.log('Bot Ready')
+
+})
+bot.login ('NDUyNDM3MDM3MDEwOTExMjMz.DfQUWA.3Ot3diWDLOW_F--afM8lysQCcng');
+
+bot.on('message', message => {
+
+    if (message.content === "*esquive"){
+        random();
+        
+        if (randnum == 1){
+            message.reply("Tu esquive l'attaque")
+            console.log("1"); 
+        }
+        if (randnum == 2){
+            message.reply("Tu te prend l'attaque '^'")
+            console.log("2"); 
+        }
+    }
+        
+}
+
+
+    
+)
+
+function random(min, max) {
+    min = Math.ceil(0);
+    max = Math.floor(2);
+    randnum = Math.floor(Math.random() * (max - min +1));
+}
